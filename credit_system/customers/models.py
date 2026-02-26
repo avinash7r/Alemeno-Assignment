e23857fe0e9b0f1c9fdaca32a0bbc6687cc5c66b
Avinash Rajure <rajureavinash7@gmail.com>
2026-02-26 14:39:50 +0530

    added customer, loan models

--- credit_system/customers/models.py
+++ credit_system/customers/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    phone_number = models.CharField(max_length=15, unique=True)
+
+    monthly_salary = models.IntegerField()
+    approved_limit = models.IntegerField()
+    current_debt = models.FloatField(default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- credit_system/loans/models.py
+++ credit_system/loans/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from customers.models import Customer
 
-# Create your models here.
+class Loan(models.Model):
+    customer = models.ForeignKey(
+        Customer,
+        on_delete=models.CASCADE,
+        related_name="loans"
+    )
+
+    loan_amount = models.FloatField()
+    interest_rate = models.FloatField()
+    tenure = models.IntegerField()
+
+    monthly_installment = models.FloatField()
+    emis_paid_on_time = models.IntegerField(default=0)
+
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Loan {self.id} - Customer {self.customer_id}"
