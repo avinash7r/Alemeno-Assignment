dda6ad939ce676059876bdf737b792fc68f9777c
Avinash Rajure <rajureavinash7@gmail.com>
2026-02-26 20:15:51 +0530

    add tests for customer registration and loan creation

--- credit_system/customers/tests.py
+++ credit_system/customers/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from customers.models import Customer
 
-# Create your tests here.
+class RegisterCustomerTest(APITestCase):
+
+    def test_register_customer(self):
+        payload = {
+            "first_name": "Avi",
+            "last_name": "Rajure",
+            "age": 22,
+            "monthly_income": 50000,
+            "phone_number": "9999999999"
+        }
+
+        response = self.client.post("/api/register", payload, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Customer.objects.count(), 1)
+
+        customer = Customer.objects.first()
+        self.assertEqual(customer.approved_limit, 1800000)
--- credit_system/loans/tests.py
+++ credit_system/loans/tests.py
@@ -1,3 +1,58 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from customers.models import Customer
+from loans.models import Loan
 
-# Create your tests here.
+
+class RegisterCustomerTest(APITestCase):
+
+    def test_register_customer(self):
+        payload = {
+            "first_name": "Avi",
+            "last_name": "Rajure",
+            "age": 22,
+            "monthly_income": 50000,
+            "phone_number": "9999999999"
+        }
+
+        response = self.client.post("/api/register", payload, format="json")
+
+        self.assertIn(
+            response.status_code,
+            [status.HTTP_200_OK, status.HTTP_201_CREATED]
+        )
+        self.assertEqual(Customer.objects.count(), 1)
+
+        customer = Customer.objects.first()
+        self.assertEqual(customer.approved_limit, 1800000)
+
+class CreateLoanTest(APITestCase):
+
+    def setUp(self):
+        self.customer = Customer.objects.create(
+            first_name="Avi",
+            last_name="Rajure",
+            age=22,
+            phone_number="8888888888",
+            monthly_salary=50000,
+            approved_limit=1800000,
+            current_debt=0
+        )
+
+    def test_create_loan(self):
+        payload = {
+            "customer_id": self.customer.id,
+            "loan_amount": 300000,
+            "interest_rate": 10,
+            "tenure": 12
+        }
+
+        response = self.client.post("/api/create-loan", payload, format="json")
+
+        self.assertIn(
+            response.status_code,
+            [status.HTTP_200_OK, status.HTTP_201_CREATED]
+        )
+        self.assertEqual(Loan.objects.count(), 1)
+        self.assertTrue(response.data["loan_approved"])
+
